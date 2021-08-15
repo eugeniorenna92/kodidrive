3c06533eb1420f0319f715f51aaab6a47ba41896
eugeniorenna92 <71562968+eugeniorenna92@users.noreply.github.com>
2021-08-15 20:44:33 +0200

    firebase hosting

--- popup.js
+++
@@ -1,6 +0,0 @@
-const y = document.getElementById("close");
-const x = document.getElementById("popup-container");
-y.addEventListener("click", () => {
-x.classList.remove("active");
-app.id="";
-});
